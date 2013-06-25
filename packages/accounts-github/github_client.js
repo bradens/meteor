41d6ee86691c40dbf16b75960c1846046273703a
Braden Simpson <braden@uvic.ca>
2013-06-25 10:12:31 -0700

    porting timhaines fix for meteor.loginwithx bug

--- packages/accounts-github/github_client.js
+++ packages/accounts-github/github_client.js
@@ -1,4 +1,9 @@
 Meteor.loginWithGithub = function(options, callback) {
+  // support a callback without options
+  if (! callback && typeof options === "function") {
+    callback = options;
+    options = null;
+  } 
   var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
   Github.requestCredential(options, credentialRequestCompleteCallback);
 };
